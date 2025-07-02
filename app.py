6678cc0dbfaef3d7dbda6c6743b81612461f9c37
TAHA2255 <tahajack421@gmail.com>
2025-07-03 01:45:16 +0500

    added project2

--- app.py
+++ app.py
@@ -162,5 +162,9 @@ def analyze():
         print("❌ Exception during processing:", str(e))
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
